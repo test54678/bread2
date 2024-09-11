47a6a6eb27bdb8077e6aa040dc5a77d963f4b905
eisaku123 <berry_eisaku@yahoo.co.jp>
2024-09-11 17:05:05 +0900

    あげてみる

--- breads/src/main.tsx
+++ breads/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './amplifyconfiguretion.json';
+Amplify.configure(amplifyconfig);
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
